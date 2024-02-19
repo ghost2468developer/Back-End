c429d6f8e500abc7bc4de37a1260edd7523fb520
OutlawDev <siyabonga.ncube669@gmail.com>
2024-02-19 16:18:13 +0200

    server up and running

--- GraphQL/src/server.ts
+++ GraphQL/src/server.ts
@@ -1,11 +1,18 @@
 import express from 'express'
-import { AppolloServer, gql } from 'apollo-server-express'
+import { ApolloServer, gql } from 'apollo-server-express'
 import resolvers from './resolvers'
 
-const typeDefs = gql'
-    ${require('fs').readFileSync(require.resolve('./schema.graphql'), 'utf8')}
-'
+const typeDefs = gql`
+  ${require('fs').readFileSync(require.resolve('./schema.graphql'), 'utf8')}
+`
 
-const server = new AppolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({ typeDefs, resolvers })
 
 const app = express()
+server.applyMiddleware({ app })
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}${server.graphqlPath}`)
+})
